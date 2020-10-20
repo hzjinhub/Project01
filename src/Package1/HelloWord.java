45ec0e339e51b2cebc5d1f5e1727f78d25554fe6
hzjin <820194081@qq.com>
2020-10-20 20:30:58 +0800

    Initial commit

---
+++ src/Package1/HelloWord.java
@@ -0,0 +1,11 @@
+package Package1;
+
+import java.util.ArrayList;
+
+public class HelloWord {
+    public static void main(String[] args){
+        ArrayList list = new ArrayList();
+        System.out.println("hello world!");
+
+    }
+}
